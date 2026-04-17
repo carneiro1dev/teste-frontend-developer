fb4df0ea99e9cd5a0a9da11c4bdee658f1da78e6
Rian Carneiro <rian.carneiro124@gmail.com>
2026-04-16 22:09:14 -0300

    Correção: Ajuste no host do banco de dados para produção

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$servidor = "://infinityfree.com";
+$servidor = "sql211.infinityfree.com";
 $usuario = "if0_41680472";
 $senha = "rian3616";
 $banco = "if0_41680472_academia";
@@ -10,5 +10,7 @@ $conexao = new mysqli($servidor, $usuario, $senha, $banco);
 if ($conexao->connect_error) {
     die("Erro de conexão: " . $conexao->connect_error);
 }
+
+$conexao->set_charset("utf8");
 
 ?>
